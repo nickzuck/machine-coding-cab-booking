a640e642d23c5b930538988a7a914ef8cef02587
nickzuck-innov <nikhil.singh@innovaccer.com>
2021-09-13 21:51:28 +0530

    Incomplete cab booking service

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+class Driver{
+
+};
+
+class Rider {
+
+};
+
+class Ride {
+
+};
+
+class CabBookingService{
+
+};
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
